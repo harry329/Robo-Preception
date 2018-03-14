75e1dcddb361fdc5be3d1d0f793d98e11c2d6dca
Harry <harsimrans329@gmail.com>
2018-03-13 20:41:14 -0700

    Initail Commit.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/catkin_ws_prj2/src
+# Build directory: /home/robond/catkin_ws_prj2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(RoboND-Perception-Project/gazebo_grasp_plugin)
+subdirs(RoboND-Perception-Project/pr2_robot)
+subdirs(RoboND-Perception-Project/pr2_moveit)
